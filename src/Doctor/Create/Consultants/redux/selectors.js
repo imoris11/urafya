1602131ef1ee58643c597fc29ddb58d3d5a4343d
Richard Igbiriki <imorobebh@gmail.com>
2020-05-03 18:30:28 +0100

    fix all tabs (#26)
    * fix all tabs
    
    * add loading

--- src/Doctor/Create/Consultants/redux/selectors.js
+++ src/Doctor/Create/Consultants/redux/selectors.js
@@ -12,11 +12,47 @@ const errorLoading = createSelector(consultations, (obj) => obj.errorLoading);
 
 const consultation = createSelector(consultations, (obj) => obj.consultation);
 
+const creatingEvaluation = createSelector(
+  consultations,
+  (obj) => obj.creatingEvaluation || false
+);
+
+const errorCreatingEvaluation = createSelector(
+  consultations,
+  (obj) => obj.errorCreatingEvaluation || false
+);
+
+const creatingDiagnosis = createSelector(
+  consultations,
+  (obj) => obj.creatingDiagnosis || false
+);
+
+const creatingPrescription = createSelector(
+  consultations,
+  (obj) => obj.creatingPrescription || false
+);
+
+const errorCreatingDiagnosis = createSelector(
+  consultations,
+  (obj) => obj.errorCreatingDiagnosis || false
+);
+
+const creatingRecommendation = createSelector(
+  consultations,
+  (obj) => obj.creatingRecommendation || false
+);
+
 const selectors = createStructuredSelector({
   consultations: getSymptoms,
   consultation,
   isLoading,
   errorLoading,
+  creatingEvaluation,
+  errorCreatingEvaluation,
+  creatingDiagnosis,
+  errorCreatingDiagnosis,
+  creatingPrescription,
+  creatingRecommendation,
 });
 
 export default selectors;
